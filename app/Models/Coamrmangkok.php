58680522a477200be3f8f41aa1afce42b0090652
kieyran <kyrann1107@gmail.com>
2025-03-24 00:41:20 +0700

    new file:   app/Models/Coamrmangkok.php 	new file:   database/migrations/2025_03_23_172239_create_coamrmangkoks_table.php

---
+++ app/Models/Coamrmangkok.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Coamrmangkok extends Model
+{
+    use HasFactory;
+    protected $table = 'tubeskelompok1posmrmangkok'; // Nama tabel eksplisit
+
+    protected $guarded = [];
+}
