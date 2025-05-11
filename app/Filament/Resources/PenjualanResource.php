af7487501e945f4922b0cf92e55fbe4fab4016d8
Mahdaldania <aldania3904@gmail.com>
2025-05-11 20:58:34 +0700

    new file:   app/Filament/Resources/PembeliResource.php new file:   app/Filament/Resources/PembeliResource/Pages/CreatePembeli.php new file:   app/Filament/Resources/PembeliResource/Pages/EditPembeli.php new file:   app/Filament/Resources/PembeliResource/Pages/ListPembelis.php new file:   app/Filament/Resources/PenjualanResource.php new file:   app/Filament/Resources/PenjualanResource/Pages/CreatePenjualan.php new file:   app/Filament/Resources/PenjualanResource/Pages/EditPenjualan.php new file:   app/Filament/Resources/PenjualanResource/Pages/ListPenjualans.php modified:   app/Models/Menu.php new file:   app/Models/Pembayaran.php new file:   app/Models/Pembeli.php new file:   app/Models/Penjualan.php new file:   app/Models/PenjualanMenu.php new file:   database/migrations/2025_05_10_162022_create_pembelis_table.php new file:   database/migrations/2025_05_10_162428_create_penjualans_table.php new file:   database/migrations/2025_05_10_163411_create_penjualan_menus_table.php new file:   database/migrations/2025_05_10_164213_create_pembayarans_table.php new file:   resources/views/penjualan-table.blade.php

---
+++ app/Filament/Resources/PenjualanResource.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PenjualanResource\Pages;
+use App\Filament\Resources\PenjualanResource\RelationManagers;
+use App\Models\Penjualan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+// Komponen Filament
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Repeater;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Hidden;
+use Filament\Tables\Filters\SelectFilter;
+
+// Model
+use App\Models\Pembeli;
+use App\Models\Menu;
+use App\Models\Pembayaran;
+use App\Models\PenjualanMenu;
+
+// DB
+use Illuminate\Support\Facades\DB;
+
+class PenjualanMenuResource extends Resource
+{
+    protected static ?string $model = Penjualan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
+    protected static ?string $navigationLabel = 'Penjualan Menu';
+    protected static ?string $navigationGroup = 'Transaksi';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Wizard::make([
+                    Wizard\Step::make('Pesanan')
+                        ->schema([
+                            Forms\Components\Section::make('Faktur')
+                                ->icon('heroicon-m-document-duplicate')
+                                ->schema([ 
+                                    TextInput::make('no_faktur')
+                                        ->default(fn () => Penjualan::getKodeFaktur())
+                                        ->label('Nomor Faktur')
+                                        ->required()
+                                        ->readonly(),
+                                    DateTimePicker::make('tgl')->default(now()),
+                                    Select::make('pembeli_id')
+                                        ->label('Pembeli')
+                                        ->options(Pembeli::pluck('nama_pembeli', 'id')->toArray())
+                                        ->required()
+                                        ->placeholder('Pilih Pembeli'),
+                                    TextInput::make('tagihan')->default(0)->hidden(),
+                                    TextInput::make('status')->default('pesan')->hidden(),
+                                ])
+                                ->collapsible()
+                                ->columns(3),
+                        ]),
+                    Wizard\Step::make('Pilih Menu')
+                        ->schema([
+                            Repeater::make('items')
+                                ->relationship('penjualanMenu')
+                                ->schema([
+                                    Select::make('menu_id')
+                                        ->label('Menu')
+                                        ->options(Menu::pluck('nama_menu', 'id')->toArray())
+                                        ->required()
+                                        ->reactive()
+                                        ->placeholder('Pilih Menu')
+                                        ->afterStateUpdated(function ($state, $set) {
+                                            $menu = Menu::find($state);
+                                            $set('harga_beli', $menu ? $menu->harga : 0);
+                                            $set('harga_jual', $menu ? $menu->harga * 1.2 : 0);
+                                        })
+                                        ->searchable(),
+                                    TextInput::make('harga_beli')
+                                        ->label('Harga Beli')
+                                        ->numeric()
+                                        ->readonly()
+                                        ->dehydrated()
+                                        ->hidden(),
+                                    TextInput::make('harga_jual')
+                                        ->label('Harga Menu')
+                                        ->numeric()
+                                        ->readonly()
+                                        ->dehydrated(),
+                                    TextInput::make('jml')
+                                        ->label('Jumlah')
+                                        ->default(1)
+                                        ->reactive()
+                                        ->required(),
+                                    DatePicker::make('tgl')
+                                        ->default(today())
+                                        ->required(),
+                                ])
+                                ->columns(['md' => 4])
+                                ->addable()
+                                ->deletable()
+                                ->reorderable()
+                                ->createItemButtonLabel('Tambah Menu')
+                                ->minItems(1)
+                                ->required(),
+                        ]),
+                    Wizard\Step::make('Pembayaran')
+                        ->schema([
+                            Placeholder::make('Tabel Pembayaran')
+                                ->content(fn (Get $get) => view('filament.components.penjualan-table', [
+                                    'pembayarans' => Penjualan::where('no_faktur', $get('no_faktur'))->get()
+                                ])), 
+                        ]),
+                ])->columnSpan(3)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('no_faktur')->label('No Faktur')->searchable(),
+                TextColumn::make('pembeli.nama_pembeli')->label('Nama Pembeli')->sortable()->searchable(),
+                TextColumn::make('status')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'bayar' => 'success',
+                        'pesan' => 'warning',
+                    }),
+                TextColumn::make('tagihan')
+                    ->formatStateUsing(fn ($state) => number_format($state, 0, ',', '.'))
+                    ->alignment('end'),
+                TextColumn::make('created_at')->label('Tanggal')->dateTime(),
+            ])
+            ->filters([
+                SelectFilter::make('status')
+                    ->label('Filter Status')
+                    ->options(['pesan' => 'Pemesanan', 'bayar' => 'Pembayaran'])
+                    ->searchable(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPenjualans::route('/'),
+            'create' => Pages\CreatePenjualan::route('/create'),
+            'edit' => Pages\EditPenjualan::route('/{record}/edit'),
+        ];
+    }
+}
